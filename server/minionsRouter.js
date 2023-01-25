9158df68fd93f1cbf20f469fbc91965ef7aaafad
Mark <103870627+Marrek1978@users.noreply.github.com>
2023-01-24 20:24:28 -0800

    working thru minionsRouter

--- server/minionsRouter.js
+++ server/minionsRouter.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const minionsRouter = express.Router();
-const {getAllFromDatabase, addToDatabase, getFromDatabaseById} = require('../server/db.js')
+const {getAllFromDatabase, addToDatabase, getFromDatabaseById, updateInstanceInDatabase } = require('../server/db.js')
 
 minionsRouter.get('/', (req, res, next) => {
     const minions = getAllFromDatabase('minions');
     res.send(minions);
 });
 
-
 minionsRouter.post('/', (req, res, next) => {
-    console.log('req.body', req.body)
     if(!req.body.name || !req.body.title || !req.body.salary){
         res.status(400).send('Minion must have a name, title, and salary');
     }else{
@@ -18,17 +16,30 @@ minionsRouter.post('/', (req, res, next) => {
     }
 })
 
-minionsRouter.get('/:minionId', (req, res, next) => {
-    const id = req.params.minionId;
+minionsRouter.param('minionId', (req, res, next, id) => {
     const minion = getFromDatabaseById('minions', id);
     if(!minion){
         res.status(404).send('Minion not found');
+    }else{
+       req.id = id;
+       req.minion = minion;
+       next();
+    }
+})
+
+minionsRouter.get('/:minionId', (req, res, next) => {
+    res.send(req.minion);
+})
+
+minionsRouter.put('/:minionId', (req, res, next) => {
+    const minion = updateInstanceInDatabase('minions', req.body);
+    if(!minion){
+        res.status(400).send('Bad Request');
     }else{
         res.send(minion);
     }
 })
 
-// GET /api/minions/:minionId to get a single minion by id.
 // PUT /api/minions/:minionId to update a single minion by id.
 // DELETE /api/minions/:minionId to delete a single minion by id.
 
