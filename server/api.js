92b3a3f30df377f1c407631d3c463ed55918a825
Mark <103870627+Marrek1978@users.noreply.github.com>
2023-01-25 17:46:30 -0800

    router path testing

--- server/api.js
+++ server/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const apiRouter = express.Router();
-const minionsRouter = require('./minionsRouter');
+const minionsRouter = require('/minionsRouter');
 const ideasRouter = require('./ideasRouter');
 const meetingsRouter = require('./meetingsRouter');
 
